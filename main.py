479040547dd19f3512bb084f13fa2ccaa20f4cef
Igor Urbanik <igorurbanik23@gmail.com>
2023-10-23 17:58:01 +0200

    Moved `example.txt` to resources

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from lech import Lech
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.m"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "resources/example.m"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
