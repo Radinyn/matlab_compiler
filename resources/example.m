1853071adeecc2ec17447c8d379d5a05bdd3e07e
Krzysztof Pęczek <peczek.kr@gmail.com>
2023-11-05 11:35:56 +0100

    Added vectors

--- resources/example.m
+++ resources/example.m
@@ -4,11 +4,11 @@ I = eye(10);    # create 10x10 matrix filled with ones on diagonal and zeros els
 D1 = A.+B' ;    # add element-wise A with transpose of B
 D2 -= A.-B' ;   # substract element-wise A with transpose of B
 D3 *= A.*B' ;   # multiply element-wise A with transpose of B
-D4 /= A./B' ; # divide element-wise A with transpose of B
+D4 /= A./B' ;   # divide element-wise A with transpose of B
 
-# E1 = [ [ 1, 2, 3],
-#        [ 4, 5, 6],
-#        [ 7, 8, 9] ];
+E1 = [ [ 1, 2, 3],
+       [ 4, 5, 6],
+       [ 7, 8, 9] ];
 
 res1 = 60.500;
 res2 = 60.;
--- src/patryk.py
+++ src/patryk.py
@@ -2,55 +2,12 @@ from sly import Parser
 from sly.yacc import YaccProduction as Production
 from lech import Lech
 
-# ================================= Expected grammar =================================
-
-# start -> program
-# program -> statement
-#
-# statement -> statement_list
-#            | expression;
-#            | PRINT expression;
-#            | assign_statement
-#            | control_statement
-#            | loop_statement
-#            | empty_statement
-#
-# expression -> ( expression )
-#             | expression PLUS           expression
-#             | expression MINUS          expression
-#             | expression TIMES          expression
-#             | expression DIVIDE         expression
-#             | expression DOT_PLUS       expression
-#             | expression DOT_MINUS      expression
-#             | expression DOT_TIMES      expression
-#             | expression DOT_DIVIDE     expression
-#             | expression EQUAL          expression
-#             | expression NOT_EQUAL      expression
-#             | expression GREATER        expression
-#             | expression GREATER_EQUAL  expression
-#             | expression LESS           expression
-#             | expression LESS_EQUAL     expression
-#             | FLOAT
-#             | INTEGER
-#
-# statement_list ->  { statement_list } | statement_list statement | statement
-#
-# assign_statement -> expression ASSIGN expression;
-#                   | expression PLUS_ASSIGN expression;
-#                   | expression MINUS_ASSIGN expression;
-#                   | expression TIMES_ASSIGN expression;
-#                   | expression DIVIDE_ASSIGN expression;
-# 
-# control_statement -> IF ( expression ) statement
-#                    | IF ( expression ) statement ELSE statement
-# 
-# loop_statement -> WHILE ( expression ) { statement }
-
 class Patryk(Parser):
     tokens = Lech.tokens
 
     precedence = (
         ('right', ASSIGN, PLUS_ASSIGN, MINUS_ASSIGN, TIMES_ASSIGN, DIVIDE_ASSIGN),
+        ('nonassoc', RANGE),
         ('left', EQUAL, NOT_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL),
         ('left', PLUS, MINUS, DOT_PLUS, DOT_MINUS),
         ('left', TIMES, DIVIDE, DOT_TIMES, DOT_DIVIDE),
@@ -62,20 +19,19 @@ class Patryk(Parser):
 
     start = 'program'
 
-    # Program
 
+    # ====== Program ======
+    
     start = 'program'
 
-    @_('statement')
+    @_('statement_list')
     def program(self, p: Production):
         return p
+    
 
-    @_('program statement')
-    def program(self, p: Production):
-        return p
-    
-    # Expressions
-    
+    # ====== Expressions ======
+
+    # Binary Expressions
     @_(
             '"(" expression ")"',
             'expression PLUS          expression',
@@ -92,15 +48,17 @@ class Patryk(Parser):
             'expression GREATER_EQUAL expression',
             'expression LESS          expression',
             'expression LESS_EQUAL    expression',
-            'expression RANGE         expression',
-            'FLOAT',
-            'INTEGER',
-            'STRING',
-            'ID'
+            'expression RANGE         expression'
     )
     def expression(self, p: Production) -> Production:
         return p
     
+    # Value expressions
+    @_('FLOAT', 'INTEGER', 'STRING', 'ID')
+    def expression(self, p: Production) -> Production:
+        return p
+    
+    # Unary Operators
     @_('MINUS expression %prec UNARY_MINUS')
     def expression(self, p: Production) -> Production:
         return p
@@ -109,30 +67,56 @@ class Patryk(Parser):
     def expression(self, p: Production) -> Production:
         return p
 
+    # Built-in functions
     @_('EYE   "(" expression ")"',
        'ZEROS "(" expression ")"',
        'ONES  "(" expression ")"')
     def expression(self, p: Production) -> Production:
         return p
 
-    # Statements
+    # ====== Expression list ======
+    @_('expression')
+    def expression_list(self, p: Production) -> Production:
+        return p
     
+    @_('expression_list "," expression')
+    def expression_list(self, p: Production) -> Production:
+        return p
+
+    # ====== Lists ======
+
+    @_('"[" expression_list "]"')
+    def vector(self, p: Production) -> Production:
+        return p
+    
+    @_('vector')
+    def expression(self, p: Production) -> Production:
+        return p
+ 
+    # ====== Statements ======
+    
+    # Standalone expression
     @_('expression ";"')
     def statement(self, p: Production) -> Production:
         return p
     
-
-
+    # Loop control
     @_('BREAK ";"',
        'CONTINUE ";"')
     def statement(self, p: Production) -> Production:
         return p
     
-    @_('PRINT expression  ";"',
-       'RETURN expression ";"')
+    # Built-in print
+    @_('PRINT expression ";"')
+    def statement(self, p: Production) -> Production:
+        return p
+
+    # Return statements
+    @_('RETURN expression ";"')
     def statement(self, p: Production) -> Production:
         return p
     
+    # Assignments
     @_(
         'ID ASSIGN          expression ";"',
         'ID PLUS_ASSIGN     expression ";"',
@@ -143,7 +127,7 @@ class Patryk(Parser):
     def statement(self, p: Production) -> Production:
         return p
 
-
+    # Control statements
     @_(
         'IF "(" expression ")" statement %prec NO_ELSE',
         'IF "(" expression ")" statement ELSE statement',
@@ -151,12 +135,15 @@ class Patryk(Parser):
     def statement(self, p: Production) -> Production:
         return p
     
+    # Loops
     @_('WHILE "(" expression ")" "{" statement "}"')
     def statement(self, p: Production) -> Production:
         return p
     
     @_('FOR "(" ID ASSIGN expression ")" "{" statement "}"')
 
+
+    # ====== Statement List ======
     @_('"{" statement_list "}"')
     def statement(self, p: Production):
         return p
